420a39b7b427286aee226673c47fa6fd6d064310
Elisariane <elisarianeBS@gmail.com>
2023-10-02 15:42:54 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let campoStatus = gameClicado.querySelector('span');
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        campoStatus.innerHTML = 'Alugar';
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        campoStatus.innerHTML = 'Devolver';
+    }
+}
